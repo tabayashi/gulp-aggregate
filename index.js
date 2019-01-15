7842bbce466e9beec8d5e328c6029cb6851f853d
tabayashi <tabayashi@gmail.com>
2019-01-16 02:39:41 +0900

    update index.js

--- index.js
+++ index.js
@@ -7,73 +7,64 @@ var PluginError   = require(' plugin-error');
 var Vinyl         = require('vinyl');
 var Q             = require('q');
 
-module.exports = function() {
-  return {
-    get: function() {
-      return function(options) {
-        var groups = {};
-        options = options || {};
+module.exports = function(options) {
+  var groups = {};
+  options = options || {};
 
-        if ('function' === kindof(options)) {
-          options = {aggregate: options};
-        }
+  if ('function' === kindof(options)) {
+    options = {aggregate: options};
+  }
 
-        if (!('group' in options) || !options.group) {
-          options.group = function(file) {
-            return path.basename(path.dirname(file.path));
-          };
-        }
+  if (!('group' in options) || !options.group) {
+    options.group = function(file) {
+      return path.basename(path.dirname(file.path));
+    };
+  }
 
-        if (!('aggregate' in options) || !options.aggregate) {
-          options.aggregate = function() {};
-        }
+  if (!('aggregate' in options) || !options.aggregate) {
+    options.aggregate = function() {};
+  }
 
-        return through.obj(
-          function(file, encoding, callback) {
-            var group;
+  return through.obj(
+    function(file, encoding, callback) {
+      var group;
 
-            if (file.isNull()) {
-              return callback(null, file);
+      if (file.isNull()) {
+        return callback(null, file);
+      }
+
+      if (file.isStream()) {
+        return callback(new PluginError('aggregate', 'Streaming not supported'));
+      }
+
+      group = options.group(file);
+      if (!(group in groups)) {
+        groups[group] = [];
+      }
+      groups[group].push(file);
+      return callback();
+    },
+    function(callback) {
+      var $this, promises;
+      $this = this;
+      promises = [];
+      Object.keys(groups).forEach(function(group) {
+        var files = options.aggregate(group, groups[group], Q.defer());
+        if (Q.isPromise(files)) {
+          promises.push(files);
+        } else {
+          files = Array.isArray(files) ? files : [files];
+          files.forEach(function(file) {
+            file = isPlainObject(file) ? new Vinyl(file) : file;
+            if (file instanceof Vinyl) {
+              $this.push(file);
             }
-
-            if (file.isStream()) {
-              return callback(new PluginError(
-                'aggregate',
-                'Streaming not supported'
-              ));
-            }
-
-            group = options.group(file);
-            if (!(group in groups)) {
-              groups[group] = [];
-            }
-            groups[group].push(file);
-            return callback();
-          },
-          function(callback) {
-            var $this, promises;
-            $this = this;
-            promises = [];
-            Object.keys(groups).forEach(function(group) {
-              var files = options.aggregate(group, groups[group], Q.defer());
-              if (Q.isPromise(files)) {
-                promises.push(files);
-              } else {
-                files = Array.isArray(files) ? files : [files];
-                files.forEach(function(file) {
-                  file = isPlainObject(file) ? new Vinyl(file) : file;
-                  if (file instanceof Vinyl) {
-                    $this.push(file);
-                  }
-                });
-              }
-            });
-            return promises.length ?
-              Q.when.apply(Q, promises).then(callback).done() :
-              callback();
-          }
-        );
-      };
+          });
+        }
+      });
+      return promises.length ?
+        Q.when.apply(Q, promises).then(callback).done() :
+        callback();
     }
-  };
+  );
 };
