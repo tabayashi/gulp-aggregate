de9c37f0be4b2bbb85c54981c39b77fb68749fea
tabayashi <tabayashi@gmail.com>
2019-01-21 00:57:31 +0900

    update

--- index.js
+++ index.js
@@ -1,70 +1,44 @@
 'use strict';
-var path          = require('path');
-var kindof        = require('kind-of');
-var through       = require('through2');
-var isPlainObject = require('is-plain-object');
-var PluginError   = require(' plugin-error');
-var Vinyl         = require('vinyl');
-var Q             = require('q');
+var path        = require('path');
+var through     = require('through2');
+var merge       = require('event-stream');
+var PluginError = require('plugin-error');
 
-module.exports = function(options) {
+module.exports = function(aggregate, options) {
   var groups = {};
+
   options = options || {};
-
-  if ('function' === kindof(options)) {
-    options = {aggregate: options};
-  }
-
   if (!('group' in options) || !options.group) {
     options.group = function(file) {
       return path.basename(path.dirname(file.path));
     };
   }
 
-  if (!('aggregate' in options) || !options.aggregate) {
-    options.aggregate = function() {};
-  }
-
   return through.obj(
-    function(file, encoding, callback) {
+    function(chunk, encoding, callback) {
       var group;
 
-      if (file.isNull()) {
-        return callback(null, file);
+      if (chunk.isNull()) {
+        return callback(null, chunk);
       }
 
-      if (file.isStream()) {
+      if (chunk.isStream()) {
         return callback(new PluginError('aggregate', 'Streaming not supported'));
       }
 
-      group = options.group(file);
+      group = options.group(chunk);
       if (!(group in groups)) {
         groups[group] = [];
       }
-      groups[group].push(file);
+      groups[group].push(chunk);
       return callback();
     },
     function(callback) {
-      var $this, promises;
-      $this = this;
-      promises = [];
-      Object.keys(groups).forEach(function(group) {
-        var files = options.aggregate(group, groups[group], Q.defer());
-        if (Q.isPromise(files)) {
-          promises.push(files);
-        } else {
-          files = Array.isArray(files) ? files : [files];
-          files.forEach(function(file) {
-            file = isPlainObject(file) ? new Vinyl(file) : file;
-            if (file instanceof Vinyl) {
-              $this.push(file);
-            }
-          });
-        }
-      });
-      return promises.length ?
-        Q.when.apply(Q, promises).then(callback).done() :
-        callback();
+      return merge.apply(null, Object.keys(groups)
+        .reduce(function(streams, group) {
+          streams.push(aggregate.call(null, group, groups[group]));
+        }, [])
+      ).on('end', callback);
     }
   );
 };
